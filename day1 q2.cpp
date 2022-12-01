ff6d30167b7fb09c1d92b8c363035878ca4b8ed4
YASHI CHAUHAN <97442087+YASHICHAUHA@users.noreply.github.com>
2022-12-01 22:17:08 +0530

    Add files via upload

---
+++ day1 q2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"enter the value of a and b and c"<<endl;
+	cin>>a>>b>>c;
+	if(a>b)
+	{
+		if(a>c)
+		
+		{ cout<<"a is greater"<<endl;}
+		else
+		{ cout<<"b is greater"<<endl;}
+	
+	}
+		else{ if (b>c) 
+		 { cout<<"b is greatest"<<endl;}
+		 else { cout<<"c is greater"<<endl;
+		 }
+	}
+		return 0;
+			
+		}
